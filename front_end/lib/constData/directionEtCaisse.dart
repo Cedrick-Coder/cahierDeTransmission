6f9ec111f2115a7a1facef90cb042f05a8e47e16
Cedrick-Coder <LuckerKreiw@gmail.com>
2026-05-21 09:47:26 +0300

    separation de donnés de caisse et agence du form_screen

---
+++ front_end/lib/constData/directionEtCaisse.dart
@@ -0,0 +1,132 @@
+class directionEtCaisse {
+    static final List<String> directions = [
+    "Direction Générale",
+    "Direction des Ressources Humaines",
+    "Direction Financière",
+    "Direction Technique",
+    "Direction Commerciale",
+    "Direction Logistique",
+    "Direction Comptabilité",
+    "Direction des Declarations",
+    "Direction Juriste",
+    "Direction Audit",
+    "Direction Marketing",
+    "Direction des Opération",
+    "Direction du Mutuelle de Santé",
+    "Direction du Responsable Controle Permanent",
+  ];
+
+  static final Map<String, List<String>> agencesEtCaisses = {
+        "Farimbontsoa": [
+      "67ha",
+      "Antohomadinika",
+      "Ampefiloha",
+      "Anosibe",
+      "Ankazomanga",
+      "Anosizato",
+    ],
+    "Aina": [
+      "Ambanidia",
+      "Ambohipo",
+      "Mandroseza",
+      "Andrefana",
+      "Soanierana",
+      "Andramasina",
+      "Arivonimamo",
+      "Miarinarivo",
+      "Soavinandriana",
+      "Tsiroanomandidy",
+    ],
+    "Vonjy": [
+      "Mahavoky",
+      "Andravoahangy",
+      "Ankadidramamy",
+      "Analamahitsy",
+      "Alarobia",
+      "Soarano",
+      "Avaradoha",
+    ],
+    "Rindra": [
+      "Mangamila",
+      "Ankazondandy",
+      "Ambatomena",
+      "Manjakandrina",
+      "Anjepy",
+      "Anjozorobe",
+    ],
+    "Mahasoa": [
+      "Ambohibao",
+      "Ambohidratrimo",
+      "Ivato",
+      "Ambohimanarina",
+      "Ambatolampy Tsimahafotsy",
+    ],
+    "Fanavotana": [
+      "Itaosy",
+      "Alasora",
+      "Andoharanofotsy",
+      "Ampitatafika",
+      "Imeritsiatosika",
+      "Tanjombato",
+    ],
+    "Fanantenana": [
+      "Ambositra",
+      "Fandriana",
+      "Fianarantsoa",
+      "Ambalavao"
+    ],
+    "Manoro": [
+      "Faratsiho",
+      "Betafo",
+      "Antsenakely",
+      "Ambatolampy",
+      "Behenjy"
+    ],
+    "Majunga": [
+      "Marovoay",
+      "Ambato-boeny",
+      "Manjarisoa"
+      "Tanambao sotema"
+    ],
+    "Betsiboka": [
+      "Maevatanana",
+      "Andriba"
+      "Manerinerina"
+    ],
+    "Anosy" : [
+      "Fort-Dauphin",
+      "Amboasary",
+      "Manambaro",
+      "Ambovombe",
+      "Tanambao",
+      "Tsihombe",
+      "Beloha",
+      "Bekily"
+    ],
+    "Alaotra Mangoro" : [
+      "Ambatondrazaka",
+      "Tanambe",
+      "Andaingo",
+      "Moramanga",
+      "Anjiro",
+      "Amparafara vola",
+      "Morarano Chrome"
+    ],
+    "Analanjirofo" : [
+      "Fenerive Est",
+      "Vavantenina",
+      "Maromitety",
+      "Soanierana Ivongo",
+      "Toamasina",
+      "Vatomandry",
+      "Foulpointe"
+    ],
+    "Sofia" : [
+      "Antsohihy",
+      "Befandriana Nord",
+      "Mandritsara",
+      "Bealanana",
+      "Ambatoriha"
+    ],
+  };
+}
