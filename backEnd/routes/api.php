4ba0a034866f84c228efc6f6fc76bf9b985310ae
Cedrick-Coder <LuckerKreiw@gmail.com>
2026-06-02 11:07:01 +0300

    b : updateFeatures, avant intergration du données biom, everything is fine

--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\API\TransmissionController;
 Route::get('/transmissions', [TransmissionController::class, 'index']);
 Route::post('/sync', [TransmissionController::class, 'store']);
 Route::put('/transmissions/{id}', [TransmissionController::class, 'update']);
+ 
